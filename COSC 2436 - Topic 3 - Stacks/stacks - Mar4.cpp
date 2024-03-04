5ead8d0f0022d4c8327cf5939e431c6598b6e30d
Work <Work@MSI>
2024-03-04 13:05:28 -0600

    Where we ended on Mar 4.

--- COSC 2436 - Topic 3 - Stacks/stacks - Mar4.cpp
+++ COSC 2436 - Topic 3 - Stacks/stacks - Mar4.cpp
@@ -1,10 +1,112 @@
 #include <iostream>
 
+#include<vector>
+
+#include<stack>
+
 using namespace std;
+
+void printStack(std::stack<char> charStack) //pass by value (versus pass by ref) 
+{
+    while (!charStack.empty())
+    {
+        cout << charStack.top();
+        charStack.pop(); //removes the top element off of the stack 
+    }
+}
+
+
+
+bool isMatch(char openSymbol, char closeSymbol)
+{
+    if (openSymbol == '(' && closeSymbol == ')')
+    {
+        return true;
+    }
+    else if (openSymbol == '{' && closeSymbol == '}')
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+
+bool isBalanced(string inputString)
+{
+    std::stack<char> openSymbolStack;
+    for (auto& currentChar : inputString) {
+        //check if current char is opening symbol
+        if (currentChar == '(' || currentChar == '{') {
+            openSymbolStack.push(currentChar);
+        }
+
+        //check for closing symbol:
+        else if (currentChar == ')' || currentChar == '}') {
+            if (openSymbolStack.empty())
+            {
+                return false; //close symbol MUST be preceded by an open symbol
+            }
+            if (isMatch(openSymbolStack.top(), currentChar))
+            {
+                openSymbolStack.pop();
+            }
+            else { //isMatch returned false, so string is unbalanced
+                return false;
+            }
+        }// add else (neither an open symbol nor a close symbol)
+    }
+
+    if (!openSymbolStack.empty())
+    {
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+}
+
 
 int main()
 {
+    //stack<int> numsStack; 
+    //cout << numsStack.top(); //AKA: "peek" in other languages 
     
+    string unbalanced = "(){(){}}"; 
+
+    cout << std::boolalpha; 
+    cout << isBalanced(unbalanced) << endl; 
+
+
+ /*   std::stack<char> palindromeStack;
+    palindromeStack.push('h');
+    palindromeStack.push('a');
+    palindromeStack.push('n');
+    palindromeStack.push('n');
+    palindromeStack.push('a');
+    palindromeStack.push('h');
+    palindromeStack.push('z');
+
+    cout << "\n" << palindromeStack.size() << endl;
+
+    printStack(palindromeStack);
+
+    cout << palindromeStack.size() << endl; */
+
+    //cout << palindromeStack.at(2) << endl;
+    //for (auto& element : palindromeStack)
+    //{
+    //    cout << element << endl; 
+    //}
+    //vector<int> v1; 
+    
+
+    
+    //main(); //infinite recursion (causes stack overflow) 
+                //see Linker -> System settings for default 1 MB value
 
     return 0;
 }
